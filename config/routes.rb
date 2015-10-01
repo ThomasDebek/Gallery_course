8fee96cb735e7351c205da19a4779dd66dfe36d9
ThomasDebek <tom.debek@gmail.com>
2015-10-01 22:13:22 +0200

    match ':controller(/:action(/:id))', :via => [:get, :post]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   #get 'test/index'
 
-  get 'strona_glowna', to: 'test#index', as: :glowna
+  #get 'strona_glowna', to: 'test#index', as: :glowna
+
+  match ':controller(/:action(/:id))', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
