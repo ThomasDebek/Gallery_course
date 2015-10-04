207ec6a32bf726483f71400ea56a08b713a7d183
ThomasDebek <tom.debek@gmail.com>
2015-10-04 21:47:58 +0200

    Render Redirect

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,4 +2,13 @@ class TestController < ApplicationController
   def index
     @testowa = "Witamy w moim kursie"
   end
+
+  def new_action
+    render ('test/witaj')
+  end
+
+  def curse
+    redirect_to('http://www.google.com')
+  end
+
 end
