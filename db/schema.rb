dfad34e931f29dccbbcb6ad0a7d9208745438d81
ThomasDebek <tom.debek@gmail.com>
2015-10-06 20:10:24 +0200

    Model artykul finisz

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006175815) do
+ActiveRecord::Schema.define(version: 20151006180507) do
+
+  create_table "artykuls", force: :cascade do |t|
+    t.integer  "strona_id"
+    t.string   "nazwa"
+    t.integer  "pozycja"
+    t.boolean  "widoczny",             default: true
+    t.text     "zawartosc"
+    t.string   "zdjecie_file_name"
+    t.string   "zdjecie_content_type"
+    t.integer  "zdjecie_file_size"
+    t.datetime "zdjecie_updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
 
   create_table "kategories", force: :cascade do |t|
     t.string   "nazwa"
