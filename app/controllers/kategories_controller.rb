1bfb8ea195185b19af6bdf82d3ea1bd58c61d43d
ThomasDebek <tom.debek@gmail.com>
2015-10-12 18:17:03 +0200

    Widok oraz kontroller new

--- app/controllers/kategories_controller.rb
+++ app/controllers/kategories_controller.rb
@@ -10,11 +10,28 @@ class KategoriesController < ApplicationController
   end
 
   def nowa
+    @kategoria = Kategorie.new({:nazwa => "Podaj nazwe kategori"})
+    @licznik = Kategorie.count + 1
   end
 
   def edycja
   end
 
   def usun
+  end
+
+  def utworz
+    @kategorie = Kategorie.new(kategorie_parametry)
+    if @kategorie.save
+      redirect_to(:action=>'index')
+    else
+      @licznik = Kategorie.count + 1
+      render 'nowa'
+    end
+  end
+
+  private
+  def kategorie_parametry
+    params.require(:kategoria).permit(:nazwa, :pozycja, :widoczna)
   end
 end
