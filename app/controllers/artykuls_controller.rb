b3c29fe72ca34b5a42d8ecb24d298af2e96b4389
ThomasDebek <tom.debek@gmail.com>
2015-11-03 22:01:01 +0100

    Nowy widok i funkcje artykulów

--- app/controllers/artykuls_controller.rb
+++ app/controllers/artykuls_controller.rb
@@ -9,6 +9,21 @@ class ArtykulsController < ApplicationController
   end
 
   def nowy
+    @artykuly = Artykul.new({:nazwa => 'Wprowadz tytul'})
+    @strona = Strona.order('pozycja ASC')
+    @licznik = Artykul.count + 1
+  end
+
+  def utworz
+    @artykuly = Strona.new(artykuly_parametry)
+    if @artykuly.save
+      flash[:notice] = "Artykuly zostały pomyslnie utworzone"
+      redirect_to(:action => 'index')
+    else
+      @licznik = Artykul.count +1
+      @strona = Strona.order('pozycja ASC')
+      render 'nowy'
+    end
   end
 
   def edycja
@@ -18,5 +33,13 @@ class ArtykulsController < ApplicationController
   end
 
   def pokaż
+  end
+
+  def nazwa
+  end
+
+  private
+  def artykuly_parametry
+    params.require(:artykuly).permit(:nazwa, :pozycja, :widoczny, :created_at, :strona_id, :zdjecie)
   end
 end
