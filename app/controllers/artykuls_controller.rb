bfcbb912272a0a474aedf8c9eed8c998b0cf0578
ThomasDebek <tom.debek@gmail.com>
2015-11-04 20:54:14 +0100

    artykuly - foto i tresc

--- app/controllers/artykuls_controller.rb
+++ app/controllers/artykuls_controller.rb
@@ -3,42 +3,62 @@ class ArtykulsController < ApplicationController
 
   layout 'admin'
 
-
   def index
     @artykuly = Artykul.sortuj
   end
 
   def nowy
-    @artykuly = Artykul.new({:nazwa => 'Wprowadz tytul'})
+    @artykuly = Artykul.new({:nazwa=>"Tytuł?"})
     @strona = Strona.order('pozycja ASC')
     @licznik = Artykul.count + 1
   end
 
   def utworz
-    @artykuly = Strona.new(artykuly_parametry)
+    @artykuly = Artykul.new(artykuly_parametry)
     if @artykuly.save
-      flash[:notice] = "Artykuly zostały pomyslnie utworzone"
-      redirect_to(:action => 'index')
+      flash[:notice] = "Artykuł został pomyślnie utworzony"
+      redirect_to(:action=>'index')
     else
-      @licznik = Artykul.count +1
+      @licznik = Artykul.count + 1
       @strona = Strona.order('pozycja ASC')
-      render 'nowy'
+      render('nowy')
     end
   end
 
   def edycja
+    @artykuly = Artykul.find(params[:id])
+    @strona = Strona.order('pozycja ASC')
+    @licznik = Artykul.count
   end
+
+  def aktualizuj
+    @artykuly = Artykul.find(params[:id])
+    if @artykuly.update_attributes(artykuly_parametry)
+      flash[:notice] = "Artykuł została pomyślnie zmodyfikowany"
+      redirect_to(:action=>'pokaz', :id => @artykuly.id)
+    else
+      @licznik = Artykul.count
+      @strona = Strona.order('pozycja ASC')
+      render('edycja')
+    end
+  end
+
+
 
   def usun
+    @artykuly = Artykul.find(params[:id])
   end
 
-  def pokaż
+  def kasuj
+    artykuly = Artykul.find(params[:id]).destroy
+    flash[:notice] = "Artykuł '#{artykuly.nazwa}' został pomyślnie usunięty"
+    redirect_to(:action=>'index')
   end
 
-  def nazwa
+  def pokaz
+    @artykuly = Artykul.find(params[:id])
   end
 
-  private
   def artykuly_parametry
     params.require(:artykuly).permit(:nazwa, :pozycja, :widoczny, :created_at, :strona_id, :zdjecie)
   end
