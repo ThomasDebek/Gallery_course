2a9db8700f854d9fa8eeacce4279004ac088367b
ThomasDebek <tom.debek@gmail.com>
2015-12-07 21:32:22 +0100

    Finish views create gallery

--- app/controllers/galeries_controller.rb
+++ app/controllers/galeries_controller.rb
@@ -8,6 +8,19 @@ class GaleriesController < ApplicationController
   end
 
   def nowa
+    @galeria = Galerie.new({:nazwa => "Wprowadz nazwę "})
+    @licznik = Galerie.count + 1
+  end
+
+  def utworz
+    @galeria = Galerie.new(galeria_parametry)
+    if @galeria.save
+      flash[:notice] = "Galeria zostala pomyslnie utworzona"
+      redirect_to(:aciton => "index")
+    else
+      @licznik = Galerie.count +1
+      render "nowa"
+    end
   end
 
   def pokaz
@@ -17,5 +30,9 @@ class GaleriesController < ApplicationController
   end
 
   def usun
+  end
+
+  def galeria_parametry
+    params.require(:galeria).permit(:nazwa, :pozycja, :widoczna, :opis, :zdjecie)
   end
 end
--- app/models/galerie.rb
+++ app/models/galerie.rb
@@ -2,6 +2,26 @@ class Galerie < ActiveRecord::Base
 
   has_many :zdjecie
 
+
+  has_attached_file :zdjecie,
+                    :styles => {:medium => "600x600>", :thumb => "200x200>" }
+
+  validates_attachment_content_type :zdjecie,
+                                    :content_type => /^image\/(png|gif|jpeg)/,
+                                    :message => '--- akceptuje tylko pliki PNG GIF i JPG ----'
+
+  validates_attachment_size :zdjecie,
+                            :in	=> 0..850.kilobytes,
+                            :message => '--- za duży plik max rozmiar to 850 kB ----'
+
+  validates  :nazwa,
+             :presence => true,
+             :length => {:maximum => 120, :message => ": Za długi tytuł galerii"}
+
+  validates :nazwa,
+            presence: true,
+            :length =>{maximum: 120, :message =>": za dlugi tekst"}
+
   scope :widoczna, lambda{where(:widoczny => true)}
   scope :niewidoczna, lambda{where(:widoczny => false)}
   scope :sortuj, lambda{order("galeries.pozycja ASC")}
