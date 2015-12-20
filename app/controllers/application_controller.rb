325a5ec9614ef8657f82c68207c067234327247d
ThomasDebek <tom.debek@gmail.com>
2015-12-20 23:14:09 +0100

    Loggin User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+
+private
+  def sprawdz_logowanie
+    unless session[:uzytkownik_id]
+      flash[:notice] = "Jestes nie zalogowany"
+      redirect_to(:controller => 'dosteps', :action => 'login')
+      return false
+    else
+      return true
+    end
+  end
+
 end
--- app/controllers/dosteps_controller.rb
+++ app/controllers/dosteps_controller.rb
@@ -3,9 +3,37 @@ class DostepsController < ApplicationController
 
   layout 'admin'
 
+  before_action :sprawdz_logowanie, :except => [:login, :logowanie, :logout]
+
   def index
   end
 
   def login
+  end
+
+  def logowanie
+    if params[:uzytkownik].present? && params[:haslo].present?
+      admin_szukam = Uzytkownik.where(:uzytkownik => params[:uzytkownik]).first
+      if admin_szukam
+        admin_autoryzacja = admin_szukam.authenticate(params[:haslo])
+      end
+    end
+    if admin_autoryzacja
+      session[:uzytkownik_id] = admin_autoryzacja.id
+      session[:uzytkownik ] = admin_autoryzacja.uzytkownik
+      flash[:notice] = "Zostałes poprawnie zalogowany"
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Blad niepoprawana nazawa uzytkownika lub hasla"
+      redirect_to(:action => 'login')
+    end
+  end
+
+
+  def logout
+    session[:uzytkownik_id] = nil
+    session[:uzytkownik] = nil
+    flash[:notice] = "Zostales wylogowany"
+    redirect_to(:action => "login")
   end
 end
