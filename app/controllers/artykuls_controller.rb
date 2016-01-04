b624768ecea9cca2ef6bb8d04d9073a5cd994216
ThomasDebek <tom.debek@gmail.com>
2016-01-04 21:45:21 +0100

    Artykuly - expansion(rozbudowa)

--- app/controllers/artykuls_controller.rb
+++ app/controllers/artykuls_controller.rb
@@ -19,8 +19,10 @@ class ArtykulsController < ApplicationController
   end
 
   def nowy
-    @artykuly = Artykul.new({:nazwa=>"Tytuł?"})
-    @strona = Strona.order('pozycja ASC')
+    @artykuly = Artykul.new({:strona_id => @strony.id, :nazwa=>"Tytuł?"})
+      # @strona = Strona.order('pozycja ASC')
+
+    @strona = @strony.kategorie.strona.sortuj
     @licznik = Artykul.count + 1
   end
 
@@ -28,7 +30,7 @@ class ArtykulsController < ApplicationController
     @artykuly = Artykul.new(artykuly_parametry)
     if @artykuly.save
       flash[:notice] = "Artykuł został pomyślnie utworzony"
-      redirect_to(:action=>'index')
+      redirect_to(:action=>'index', :strona_id => @strony.id)
     else
       @licznik = Artykul.count + 1
       @strona = Strona.order('pozycja ASC')
@@ -46,7 +48,7 @@ class ArtykulsController < ApplicationController
     @artykuly = Artykul.find(params[:id])
     if @artykuly.update_attributes(artykuly_parametry)
       flash[:notice] = "Artykuł została pomyślnie zmodyfikowany"
-      redirect_to(:action=>'pokaż', :id => @artykuly.id)
+      redirect_to(:action=>'pokaż', :id => @artykuly.id, :strona_id => @strony.id)
     else
       @licznik = Artykul.count
       @strona = Strona.order('pozycja ASC')
@@ -63,7 +65,7 @@ class ArtykulsController < ApplicationController
   def kasuj
     artykuly = Artykul.find(params[:id]).destroy
     flash[:notice] = "Artykuł '#{artykuly.nazwa}' został pomyślnie usunięty"
-    redirect_to(:action=>'index')
+    redirect_to(:action=>'index', :strona_id => @strony.id)
   end
 
   def pokaż
@@ -72,7 +74,7 @@ class ArtykulsController < ApplicationController
 
   private
   def artykuly_parametry
-    params.require(:artykuly).permit(:nazwa, :pozycja, :widoczny, :created_at, :strona_id, :zdjecie)
+    params.require(:artykuly).permit(:nazwa, :pozycja, :widoczny, :created_at, :strona_id, :zdjecie, :zawartosc)
   end
 
   def szukaj_strony
